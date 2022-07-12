3289e54a1323b8e3c18838019a5f80d0e17aa18d
Boi Schrader <boi1992@t-online.de>
2022-07-12 12:56:01 +0200

    task done

---
+++ solution.js
@@ -0,0 +1,20 @@
+let i = 9;
+while(i<15){
+    console.log(i);
+    i++;
+}
+
+let sum=0;
+let n=1;
+while(n<21){
+    sum+=n;
+    n++;
+}
+console.log("the sum of numbers from 1-20", sum)
+
+i=11;
+do{
+    console.log('the number is', i);
+    i++;
+}
+while(i<20)
